bbf87e255a5c467c3c2807df07697ff0e2531a13
FreezyXV <i_841@mail.ru>
2025-09-18 10:46:40 +0200

    Fix service container bindings for WorkoutService dependencies

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -18,6 +18,31 @@ class AppServiceProvider extends ServiceProvider
         $this->app->singleton(\App\Services\CacheService::class, function ($app) {
             return new \App\Services\CacheService();
         });
+
+        // Register CalorieCalculatorService as singleton
+        $this->app->singleton(\App\Services\CalorieCalculatorService::class, function ($app) {
+            return new \App\Services\CalorieCalculatorService();
+        });
+
+        // Register StreakCalculatorService as singleton
+        $this->app->singleton(\App\Services\StreakCalculatorService::class, function ($app) {
+            return new \App\Services\StreakCalculatorService();
+        });
+
+        // Register StatisticsService as singleton (depends on StreakCalculatorService)
+        $this->app->singleton(\App\Services\StatisticsService::class, function ($app) {
+            return new \App\Services\StatisticsService(
+                $app->make(\App\Services\StreakCalculatorService::class)
+            );
+        });
+
+        // Register WorkoutService as singleton (depends on CalorieCalculatorService and StatisticsService)
+        $this->app->singleton(\App\Services\WorkoutService::class, function ($app) {
+            return new \App\Services\WorkoutService(
+                $app->make(\App\Services\CalorieCalculatorService::class),
+                $app->make(\App\Services\StatisticsService::class)
+            );
+        });
     }
 
     public function boot()
